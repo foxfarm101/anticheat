7d73ca19da1fcae6409e71a40ea9065008a810e3
foxfarm101 <kookiekat707@gmail.com>
2026-09-15 10:36:29 -0400

    finish c++ side of the C++ <-> Java bridge

--- bridge/wire.hpp
+++ bridge/wire.hpp
@@ -2,6 +2,7 @@
 #include "anticheat/event.hpp"
 #include <cstddef>
 #include <cstdint>
+
 namespace ac{
-Event decode_event(const std::uint8_t* bytes, std::size_t size);
+    Event decode_event(const std::uint8_t* bytes, std::size_t size);
 }
